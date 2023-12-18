4b3cf3613bd78ae4e7cf967f63be912ecdb56eae
Dirk <112545979+Diirkk@users.noreply.github.com>
2023-12-18 11:23:50 +0000

    costomize webpage

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Dirk Goldschmitt",
+  title: "Postgraduate researcher",
+  email: "dgoldschmitt1@sheffield.ac.uk",
+  gitHub: "Diirkk",
+  linkedIn: "dirkgoldschmitt",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
